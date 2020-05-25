525442863417763438984232c3587dc4f5af6989
JOJAEHYEOK <jjhst2285@naver.com>
2020-05-25 19:51:23 +0900

    test1

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['django1-env.eba-aztcpqee.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django2-env.eba-aztcpqee.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
